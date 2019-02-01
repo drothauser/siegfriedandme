510d3fb5a11b6c3b4939e5350b158a5a20254e9b
Douglas Rothauser <drothauser@yahoo.com>
2019-02-01 09:11:57 -0500

    development

---
+++ menu.js
@@ -0,0 +1,8 @@
+// Side bar menu - See https://www.w3schools.com/howto/howto_js_sidenav.asp
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
