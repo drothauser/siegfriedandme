2d02aa6edfad9a5d9c0e92c67f257bcf66117960
Douglas Rothauser <drothauser@yahoo.com>
2019-02-02 18:36:34 -0500

    add content

--- menu.js
+++ menu.js
@@ -6,3 +6,11 @@ function openNav() {
 function closeNav() {
     document.getElementById("menu").style.width = "0";
 }
+
+var menuHTML = ''
+menuHTML += '<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>'
+menuHTML += '<a href="index.html">Home</a>'
+menuHTML += '<a href="assertions.html">Assertions &amp; Answers</a>'
+menuHTML += '<a href="performances.html">Performances</a>'
+menuHTML += '<a href="contact.html">Contact</a>'
+document.write(menuHTML)  
