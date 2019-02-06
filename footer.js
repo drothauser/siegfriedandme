ba050933322c59abd5057db9f39f51e4e03483e3
Douglas Rothauser <drothauser@yahoo.com>
2019-02-05 20:00:42 -0500

    development

---
+++ footer.js
@@ -0,0 +1,12 @@
+// Footer section - See https://www.w3schools.com/howto/howto_js_sidenav.asp
+
+var footerHTML = ''
+footerHTML += '<hr/>'
+footerHTML += '<div>'
+footerHTML += '<a href="index.html">Home</a> | '
+footerHTML += '<a href="assertions.html">Assertions</a> | '
+footerHTML += '<a href="performances.html">Performances</a> |'
+footerHTML += '<a href="contact.html">Contact</a>'
+footerHTML += '</div>'
+
+document.write(footerHTML) 
---
+++ header.js
@@ -0,0 +1,12 @@
+// Header section - See https://www.w3schools.com/howto/howto_js_sidenav.asp
+
+var headerHTML = ''
+headerHTML += '<a class="header" href="index.html">'
+headerHTML += '<h1>SIEGFRIED &amp; ME</h1>'
+headerHTML += '<h2>The Best of Atheism Answered</h2>'
+headerHTML += '<div>'
+headerHTML += '<img src="images/siegfriedandme.png" alt="Siegried And Me - The Best of Atheism Answered" title="Siegried And Me - The Best of Atheism Answered">'
+headerHTML += '</div>'
+headerHTML += '</a>'
+
+document.write(headerHTML)  
