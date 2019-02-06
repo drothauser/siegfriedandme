6a87d1ce4cc61fdfbb777586d76a6547e3c54e94
Douglas Rothauser <drothauser@yahoo.com>
2019-02-05 20:11:32 -0500

    development

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,4 @@
 
-aws s3 cp . s3://%1/  --recursive --exclude "*" --include "*.html" 
-aws s3 cp styles.css s3://%1/  
-aws s3 cp menu.js s3://%1/  
+aws s3 cp . s3://siegfriedandme.%1/  --recursive --exclude "*" --include "*.html" 
+aws s3 cp . s3://siegfriedandme.%1/  --recursive --exclude "*" --include "*.js" 
+aws s3 cp . s3://siegfriedandme.%1/  --recursive --exclude "*" --include "*.css" 
