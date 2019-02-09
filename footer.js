b81be29af86760359ce0c2f419ef9a8a7ff8f9b1
Douglas Rothauser <drothauser@yahoo.com>
2019-02-09 15:45:50 -0500

    add Bible links

--- footer.js
+++ footer.js
@@ -5,8 +5,17 @@ footerHTML += '<hr/>'
 footerHTML += '<div>'
 footerHTML += '<a href="index.html">Home</a> | '
 footerHTML += '<a href="assertions.html">Assertions</a> | '
-footerHTML += '<a href="performances.html">Performances</a> |'
+footerHTML += '<a href="performances.html">Performances</a> | '
 footerHTML += '<a href="contact.html">Contact</a>'
 footerHTML += '</div>'
 
 document.write(footerHTML) 
+
+var bibleLinks = ''
+bibleLinks += '<script src="https://www.biblegateway.com/public/link-to-us/tooltips/bglinks.js" type="text/javascript"></script>'
+bibleLinks += '<script type="text/javascript">'
+bibleLinks += '    BGLinks.version = "ESV";'
+bibleLinks += '    BGLinks.linkVerses();'
+bibleLinks += '</script>'
+
+document.write(bibleLinks) 
