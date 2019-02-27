af65d767b78d353e32ed212c34c58a32ba9469c3
Douglas Rothauser <drothauser@yahoo.com>
2019-02-26 19:43:25 -0500

    edit content

--- deploy.bat
+++ deploy.bat
@@ -2,3 +2,5 @@
 aws s3 cp . s3://siegfriedandme.%1/  --recursive --exclude "*" --include "*.html" 
 aws s3 cp . s3://siegfriedandme.%1/  --recursive --exclude "*" --include "*.js" 
 aws s3 cp . s3://siegfriedandme.%1/  --recursive --exclude "*" --include "*.css" 
+aws s3 cp . s3://siegfriedandme.%1/  --recursive --exclude "*" --include "videos/*.mp4" 
+aws s3 cp . s3://siegfriedandme.%1/  --recursive --exclude "*" --include "images/*.jpg" 
