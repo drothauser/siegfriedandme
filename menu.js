f7d9d052db59554cc4542de03c8717435ec55564
Douglas Rothauser <drothauser@yahoo.com>
2019-04-13 22:32:25 -0400

    videos

--- menu.js
+++ menu.js
@@ -1,5 +1,14 @@
 // Side bar menu - See https://www.w3schools.com/howto/howto_js_sidenav.asp
 function openNav() {
+    document.getElementById("menu").style.width = "550px";
+}
+
+function closeNav() {
+    document.getElementById("menu").style.width = "0";
+}
+
+// Side bar menu - See https://www.w3schools.com/howto/howto_js_sidenav.asp
+function openNav() {
     document.getElementById("menu").style.width = "250px";
 }
 
@@ -13,4 +22,19 @@ menuHTML += '<a href="index.html">Home</a>'
 menuHTML += '<a href="assertions.html">Assertions &amp; Answers</a>'
 menuHTML += '<a href="performances.html">Performances</a>'
 menuHTML += '<a href="contact.html">Contact</a>'
+
+//menuHTML += '<a href="assertion1.html">All Christians Are Atheists</a>'
+//menuHTML += '<a href="assertion2.html">A Universe from Nothing</a>'
+//menuHTML += '<a href="assertion3.html">A Complex God Would Need an Explanation</a>'
+//menuHTML += '<a href="assertion4.html">Government</a>'
+//menuHTML += '<a href="assertion5.html">The Problem Of Evil</a>'
+//menuHTML += '<a href="assertion6.html">God and science - Darwin and dating</a>'
+//menuHTML += '<a href="assertion7.html">The Garden as a Setup</a>'
+//menuHTML += '<a href="assertion8.html">God enslaved the Jews</a>'
+//menuHTML += '<a href="assertion9.html">God is a moral monster</a>'
+//menuHTML += '<a href="assertion10.html">The Christian Story of Who Jesus Is Can&apos;t Be Trusted</a>'
+//menuHTML += '<a href="assertion11.html">Siegfried&apos;s invitation: Imagine there&apos;s no Heaven...</a>'
+//menuHTML += '<a href="assertion12.html">God Is an Egomaniacal Child-Abuser</a>'
+
 document.write(menuHTML)  
+
